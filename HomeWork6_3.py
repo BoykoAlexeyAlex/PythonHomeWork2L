62a733a1ea3460ad4da7996e7d15dda88a1cbfc4
BoykoAlexeyAlex <93516080+BoykoAlexeyAlex@users.noreply.github.com>
2021-11-19 18:55:10 +0200

    Add files via upload

--- HomeWork6_3.py
+++ HomeWork6_3.py
@@ -4,9 +4,9 @@ str1 = ''
 for i in range(h):
     for j in range(h*2-1):
         if i == h-1:
-            str1 += '*'
+            str1 += '* '
         elif j == (h-1+i) or j == (h-1-i):
-            str1 += '*'
-        else: str1 += ' '
+            str1 += '* '
+        else: str1 += '  '
     print(str1)
     str1 = ''
