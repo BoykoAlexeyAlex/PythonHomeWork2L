5986e53f5056f377e8853330d60e9817c15e366a
BoykoAlexeyAlex <93516080+BoykoAlexeyAlex@users.noreply.github.com>
2021-11-19 21:25:41 +0200

    Add files via upload

--- HomeWork6_5.py
+++ HomeWork6_5.py
@@ -2,7 +2,7 @@ h= int(input('Введите высоту фигуры в символах (не
 str1 = ''
 for i in range(h):
     for j in range(h):
-        if j >= (h//2-i) and j <= (h//2+i) and i<=(h)//2 and i<=h//2:
+        if j >= (h//2-i) and j <= (h//2+i)  and i<=h//2:
             str1 += '* '
         elif i >= h//2 and j==i-h//2 or j==h-1-(i-h//2):
             str1 +='* '
