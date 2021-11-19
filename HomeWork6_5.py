d6de8cd745708d973732164adc28cae6f1baaeb2
BoykoAlexeyAlex <93516080+BoykoAlexeyAlex@users.noreply.github.com>
2021-11-19 21:08:47 +0200

    Add files via upload

--- HomeWork6_5.py
+++ HomeWork6_5.py
@@ -1,14 +1,10 @@
-h= int(input('Введите высоту фигуры в символах (нечетное количество) '))+1
+h= int(input('Введите высоту фигуры в символах (нечетное количество) '))
 str1 = ''
-t1 = 0
-t2 = 0
-for i in range(h-1):
-    for j in range(h-1):
-        t1 = i-h//2+1
-        t2 = (h-1-i)
-        if j >= (h//2-1-i) and j <= (h//2-1+i) and i<=(h-1)//2 and i<=h//2:
+for i in range(h):
+    for j in range(h):
+        if j >= (h//2-i) and j <= (h//2+i) and i<=(h)//2 and i<=h//2:
             str1 += '* '
-        elif i >= h//2 and j==i-h//2+1 or j==h-2-(i-h//2+1):
+        elif i >= h//2 and j==i-h//2 or j==h-1-(i-h//2):
             str1 +='* '
         else: str1 += '  '
     print(str1)
